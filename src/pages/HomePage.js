55f70bcca06f6477b72d55c9ae08ea2dec7975e8
aggutierrez <agustinguti123@gmail.com>
2021-07-28 17:19:52 -0300

    Arreglando pequeño conflicto

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Modal } from '../components/Modal'
 
 export const HomePage = () => {
     return (
         <div>
-            <Modal />
+
         </div>
     )
 }
