218f4d4fb16b87748241b6abb85e0e5aa110f26e
aggutierrez <agustinguti123@gmail.com>
2021-07-29 21:22:19 -0300

    Arreglando conflictos

--- src/components/genero/FormAgregarGenero.js
+++ src/components/genero/FormAgregarGenero.js
@@ -10,39 +10,39 @@ const initialState = {
     nombre: "",
 };
 
-// export const FormAgregarGenero = () => {
+export const FormAgregarGenero = () => {
 
-//     const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-//     const [formValues, handleInputChange, reset] = useForm(initialState);
+    const [formValues, handleInputChange, reset] = useForm(initialState);
 
-//     const { nombre } = formValues;
+    const { nombre } = formValues;
 
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         dispatch(startCrearGenero(nombre));
-//         reset();
-//         dispatch(cerrarModal());
-//     };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(startCrearGenero(nombre));
+        reset();
+        dispatch(cerrarModal());
+    };
 
 
     return (
         <div>
 
             <form onSubmit={handleSubmit}>
-                
+
                 <label for="nombre" class="colocar_nombre">Agregar un nuevo género<span>*</span></label>
 
                 <div className="formGenero">
                     <tr>
-                    <td><input type="text" name="nombre" onChange={handleInputChange} value={nombre} placeholder="¿Cuál es el nuevo género?" autoComplete="off"></input></td>
-                    <td><button type="submit"> Ingresar género </button></td>
-                    </tr>           
+                        <td><input type="text" name="nombre" onChange={handleInputChange} value={nombre} placeholder="¿Cuál es el nuevo género?" autoComplete="off"></input></td>
+                        <td><button type="submit"> Ingresar género </button></td>
+                    </tr>
                 </div>
-                
-                
+
+
             </form>
-            
+
 
             <p class="aviso"><span> * </span>Campos obligatorios.</p>
 
