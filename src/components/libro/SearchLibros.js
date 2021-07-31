306a549035f12f49042f848898dd6603ed18a31d
Janegro09 <javier.sosa099@gmail.com>
2021-07-30 23:33:01 -0300

    se corrigen botones

--- src/components/libro/SearchLibros.js
+++ src/components/libro/SearchLibros.js
@@ -53,7 +53,6 @@ export const SearchLibros = () => {
             <button
                 type="submit"
                 className="bottonLibro"
-                
             >
                 Buscar...
             </button>
